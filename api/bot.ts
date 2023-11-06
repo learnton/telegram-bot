5a61879e31b00c8e43784716d9cf4748ea990c90
refined-x <tower1229@foxmail.com>
2023-11-06 15:26:25 +0800

    feat: add Keyboard

--- api/bot.ts
+++ api/bot.ts
@@ -1,4 +1,4 @@
-import { Bot } from "https://deno.land/x/grammy@v1.8.3/mod.ts";
+import { Bot, Keyboard } from "https://deno.land/x/grammy@v1.8.3/mod.ts";
 
 const token = Deno.env.get("BOT_TOKEN") || "";
 if (!token) throw new Error("BOT_TOKEN is unset");
@@ -10,7 +10,8 @@ bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
 // 处理其他的消息。
 bot.on("message", (ctx) => ctx.reply("Got another message!"));
 
-// 现在，你已经确定了将如何处理信息，可以开始运行你的 bot。
-// 这将连接到 Telegram 服务器并等待消息。
+new Keyboard()
+  .webApp("webApp", "https://learnton.github.io/twa-vite/")
+  .persistent();
 
 export default bot;
