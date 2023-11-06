d76a3c895cfa734f71c2a306f9c48ad9c17d0828
refined-x <tower1229@foxmail.com>
2023-11-06 15:34:16 +0800

    fix: send Keyboard with start

--- api/bot.ts
+++ api/bot.ts
@@ -6,12 +6,15 @@ if (!token) throw new Error("BOT_TOKEN is unset");
 const bot = new Bot(token);
 
 // 处理 /start 命令。
-bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
+bot.command("start", (ctx) =>
+  ctx.reply("Welcome! Up and running.", {
+    reply_markup: new Keyboard().webApp(
+      "webApp",
+      "https://learnton.github.io/twa-vite/"
+    ),
+  })
+);
 // 处理其他的消息。
 bot.on("message", (ctx) => ctx.reply("Got another message!"));
-
-new Keyboard()
-  .webApp("webApp", "https://learnton.github.io/twa-vite/")
-  .persistent();
 
 export default bot;
