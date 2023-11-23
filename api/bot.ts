d2582c0279259a8f017e75ae33fc5e0413048bf7
tower1229 <tower1229@foxmail.com>
2023-11-23 17:57:10 +0800

    feat: update keyboard

--- api/bot.ts
+++ api/bot.ts
@@ -1,4 +1,4 @@
-import { Bot, Keyboard } from "https://deno.land/x/grammy@v1.8.3/mod.ts";
+import { Bot, InlineKeyboard } from "https://deno.land/x/grammy@v1.8.3/mod.ts";
 
 const token = Deno.env.get("BOT_TOKEN") || "";
 if (!token) throw new Error("BOT_TOKEN is unset");
@@ -8,13 +8,24 @@ const bot = new Bot(token);
 // 处理 /start 命令。
 bot.command("start", (ctx) =>
   ctx.reply("Welcome! Up and running.", {
-    reply_markup: new Keyboard().webApp(
+    reply_markup: new InlineKeyboard().webApp(
       "ton-webapp",
       "https://learnton.github.io/ton-webapp/"
     ),
   })
 );
 // 处理其他的消息。
-bot.on("message", (ctx) => ctx.reply("Got another message!"));
+bot.on("message", (ctx) => {
+  switch (ctx.message) {
+    case "clear":
+      ctx.reply("will clear keyboard", {
+        reply_markup: { remove_keyboard: true },
+      });
+
+      break;
+    default:
+      ctx.reply("Got:" + ctx.message);
+  }
+});
 
 export default bot;
