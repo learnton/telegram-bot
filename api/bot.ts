edf288bf392d586a7a243cd21e492e597505f311
tower1229 <tower1229@foxmail.com>
2023-12-15 22:58:22 +0800

    fix: try

--- api/bot.ts
+++ api/bot.ts
@@ -10,31 +10,19 @@ if (!token) throw new Error("BOT_TOKEN is unset");
 const bot = new Bot(token);
 
 // 处理 /start 命令。
-bot.command("start", (ctx) =>
-  ctx.reply("Welcome! Up and running.", {
-    reply_markup: new InlineKeyboard().webApp(
-      "ton-webapp",
-      "https://ton.zkid.xyz/"
-    ),
-  })
-);
-bot.command("card", (ctx) =>
+bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
+bot.command("card", (ctx) => {
+  const keyboard = new InlineKeyboard().webApp(
+    "pick a card",
+    "https://ton.zkid.xyz/cards"
+  );
   ctx.reply("Want show your card?", {
-    reply_markup: new InlineKeyboard().webApp(
-      "pick a card first",
-      "https://ton.zkid.xyz/cards"
-    ),
-  })
-);
+    reply_markup: keyboard,
+  });
+});
 // 处理其他的消息。
 bot.on("message", (ctx) => {
   switch (ctx.message.text) {
-    case "clear":
-      ctx.reply("will clear keyboard", {
-        reply_markup: { remove_keyboard: true },
-      });
-
-      break;
     case "card":
       ctx.replyWithPhoto(
         "https://zcloak.s3.us-east-2.amazonaws.com/prod/1694514129471_mEMGaae66Z.png",
@@ -58,9 +46,7 @@ bot.inlineQuery(/card/, async (ctx) => {
     "id:test1",
     "https://zcloak.s3.us-east-2.amazonaws.com/prod/1694514129471_mEMGaae66Z.png",
     {
-      title: "Card",
       caption: "<b>Membership Card</b>",
-      description: "Membership Card description",
       parse_mode: "HTML",
       reply_markup: new InlineKeyboard().url(
         "Check this card",
