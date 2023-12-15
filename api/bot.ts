9a51e3dac74ea6d7aadbd5d2730ded6401d48ff8
tower1229 <tower1229@foxmail.com>
2023-12-15 20:22:44 +0800

    fix: add test

--- api/bot.ts
+++ api/bot.ts
@@ -32,6 +32,26 @@ bot.on("message", (ctx) => {
   }
 });
 
+bot.inlineQuery(/test1 (framework|library)/, async (ctx) => {
+  const result = InlineQueryResultBuilder.article(
+    "id:show-proof",
+    "Show proof",
+    {
+      reply_markup: new InlineKeyboard().url(
+        "Check this card",
+        "https://card.zkid.app/"
+      ),
+    }
+  ).photo("id-0", "https://grammy.dev/images/grammY.png", {
+    title: "test title",
+    description: "test description",
+    caption: "test caption",
+  });
+
+  // 回复 inline query.
+  await ctx.answerInlineQuery([result]);
+});
+
 bot.on("inline_query", async (ctx) => {
   // 创建一个单独的 inline query 结果。
   const result = InlineQueryResultBuilder.article(
@@ -50,10 +70,7 @@ bot.on("inline_query", async (ctx) => {
   });
 
   // 回复 inline query.
-  await ctx.answerInlineQuery(
-    [result], // 用结果列表回复
-    { cache_time: 30 * 24 * 3600 } // 30 天的秒数
-  );
+  await ctx.answerInlineQuery([result]);
 });
 
 export default bot;
