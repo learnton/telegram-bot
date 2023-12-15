680550ce9762510ba05f43631b0b43ee49ac9c7c
tower1229 <tower1229@foxmail.com>
2023-12-15 18:55:23 +0800

    feat: test inline_query

--- api/bot.ts
+++ api/bot.ts
@@ -1,4 +1,8 @@
-import { Bot, InlineKeyboard } from "https://deno.land/x/grammy@v1.8.3/mod.ts";
+import {
+  Bot,
+  InlineKeyboard,
+  InlineQueryResultBuilder,
+} from "https://deno.land/x/grammy@v1.8.3/mod.ts";
 
 const token = Deno.env.get("BOT_TOKEN") || "";
 if (!token) throw new Error("BOT_TOKEN is unset");
@@ -26,6 +30,30 @@ bot.on("message", (ctx) => {
     default:
       ctx.reply("Got:" + ctx.message.text);
   }
+});
+
+bot.on("inline_query", async (ctx) => {
+  // 创建一个单独的 inline query 结果。
+  const result = InlineQueryResultBuilder.article(
+    "id:grammy-website",
+    "grammY",
+    {
+      reply_markup: new InlineKeyboard().url(
+        "grammY website",
+        "https://grammy.dev/"
+      ),
+    }
+  ).text(
+    `<b>grammY</b> is the best way to create your own Telegram bots.
+They even have a pretty website! 👇`,
+    { parse_mode: "HTML" }
+  );
+
+  // 回复 inline query.
+  await ctx.answerInlineQuery(
+    [result], // 用结果列表回复
+    { cache_time: 30 * 24 * 3600 } // 30 天的秒数
+  );
 });
 
 export default bot;
